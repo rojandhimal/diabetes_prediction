f822f281003a82cf88acf36cf672eb530bf19e2f
unknown <rojandhimal1@gmail.com>
2021-02-10 11:26:22 +0545

    diabetis prediction

--- register_login/accounts/views.py
+++ register_login/accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+import pickle
+import numpy as np 
+import pandas as pd
+from django.templatetags.static import static
 
 # Create your views here.
 from .forms import RegisterForm
@@ -40,7 +44,40 @@ def loginUser(request):
     return render(request, 'accounts/login.html', {})
 
 def home(request):
-    return render(request, 'accounts/home.html', {})
+    context = {}
+    if request.POST:
+        Pregencies = float(request.POST.get('Pregencies'))
+        Glucose = float(request.POST.get('Glucose'))
+        BloodPressure = float(request.POST.get('BloodPressure'))
+        SkinThickness = float(request.POST.get('SkinThickness'))
+        Insulin = float(request.POST.get('Insulin'))
+        BMI = float(request.POST.get('BMI'))
+        DiabetesPedigreeFunction = float(request.POST.get('DiabetesPedigreeFunction'))
+        Age = float(request.POST.get('Age'))
+        context = {'Pregencies':[Pregencies],
+        'Glucose':[Glucose],
+        'BloodPressure':[BloodPressure],
+        'SkinThickness':[SkinThickness],
+        'Insulin':[Insulin],
+        'BMI':[BMI],
+        'DiabetesPedigreeFunction':[DiabetesPedigreeFunction],
+        'Age':[Age]
+        }
+
+        data = pd.DataFrame.from_dict(context)
+        res = ''
+        with open('GNB.pkl', 'rb') as f:
+            GNB = pickle.load(f)
+            res = GNB.predict(data)
+            res_prob = GNB.predict_log_proba(data)
+        predict = ''
+        if res ==1:
+            predict = '+ve'
+        else:
+            predict= '-ve'
+        context.update({'res':predict, 'res_prob':res_prob})
+
+    return render(request, 'accounts/home.html', context)
 
 
 def logoutUser(request):
